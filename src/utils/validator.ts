f8b7b56b3758bdb85a47c72aab7470e3b61a07b3
vahghar <raghav10g@gmail.com>
2025-07-24 11:58:16 +0530

    small-fix

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
+//this is to validate when creating a new task
 export const taskSchema = z.object({
   title: z.string().min(1, 'Title is required'),
   description: z.string().min(1, 'Description is required'),
   status: z.enum(['PENDING', 'IN_PROGRESS', 'COMPLETED'])
 });
 
+//this is to validate the queries
 export const querySchema = z.object({
   q: z.string().optional(),
   status: z.string().optional(),
